66a375d09fa2230b3aeba917a49aabe1a87b8ff0
Jonalogy <jonalogy@gmail.com>
2016-11-29 11:52:37 +0800

    Looping through taxi-availability array to give a false sense of movement. This is only a temporary measure

--- map.js
+++ map.js
@@ -10,6 +10,8 @@ var tracker_map;
 var radMark, clientMark;
 var bounds;
 
+var i = 0;
+
 //----Callback InitMap()
 function initMap(){
   tracker_map = new google.maps.Map(document.getElementById('map'), mapStart);
@@ -40,8 +42,11 @@ function initMap(){
 }
 
 //----Updating of RedMartian's location
-setInterval(getVehCoords, 10000) //Polling taxi data
+setInterval(getVehCoords, 1000) //Polling taxi data
 function getVehCoords(){
+
+  i++;
+
   var d = (new Date()).toISOString();
   var iso_date = d.substring(0 , 19)
 
@@ -54,7 +59,7 @@ function getVehCoords(){
       method: "GET",
       headers: { 'api-key': 'ezrYi8fQCFNhMc21SdHdcqqhzSApjgGP'}
     }).done(function(data){
-      var taxi = data.features[0].geometry.coordinates[100];
+      var taxi = data.features[0].geometry.coordinates[i];
       taxiLatLng = { lat: taxi[1], lng: taxi[0] }
       console.log("RadMartian at: ", taxiLatLng);
       change_pos(taxiLatLng)
@@ -73,69 +78,3 @@ function getVehCoords(){
     tracker_map.setCenter(centerBounds);
   }
 }//END function getVehCoords
-
-
-
-
-// //----- DOMContentLoaded events
-// document.addEventListener("DOMContentLoaded" , function(event){
-//   console.log("DOMContentLoaded")
-//
-// })
-
-
-//-------initMap() for multiple Markers
-
-  // function initMap(){
-  //   console.log("Callback: initMap");
-  //
-  //   var largeInfowindow = new google.maps.InfoWindow();
-  //   var bounds = new google.maps.LatLngBounds();
-  //
-  //   //Map Setup
-  //   tracker_map = new google.maps.Map( document.getElementById('map') , mapStart );
-  //   var redmart = { lat: 1.312623, lng: 103.7225139 }
-  //
-  //   // Markers
-  //   var markers = []; //creating a blank marker
-  //   var locations = [
-  //     { title: "Jurong Point", position: {lat: 1.339509, lng: 103.7058107 } },
-  //     { title: "Lakeshore Condominium", position: {lat: 1.3457178, lng: 103.7226515 } },
-  //     { title: "Regent Heights", position: {lat: 1.3525186, lng: 103.7559745 } }
-  //   ];
-  //
-  //   var i = 0;
-  //   locations.forEach( function(location) {
-  //     i++;
-  //     var marker = new google.maps.Marker(
-  //       { map: tracker_map,
-  //         position: location.position,
-  //         title: location.title,
-  //         animation: google.maps.Animation.DROP,
-  //         id: 1 }
-  //     );
-  //     // Push the marker to our array of markers.
-  //     markers.push(marker)
-  //     // Extend the boundaries of the map for each marker
-  //     bounds.extend(marker.position);
-  //     // Create and onclick event to open an infowindow at each marker.
-  //     marker.addListener('click' , function(){
-  //       populateInfoWindow( this, largeInfowindow )
-  //     })
-  //   }) //END locations.forEach()
-  //
-  //   map.fitBounds(bounds);
-  // } //END initMap
-
-  // function populateInfoWindow(){
-  //   // Check to make sure the infowindow si not already opened on this marker.
-  //   if(infowindow.marker != marker) {
-  //     infowindow.amrker = marker;
-  //     infowindow.setContent('<div>' + marker.title + '</div>');
-  //     infowindow.open(map, marker);
-  //     // Make sure the marker property is cleared if the infowindow is closed.
-  //     infowindow. addListener('closeclick' ,function(){
-  //       infowindow.setMarker(null);
-  //     })
-  //   } //END if(infowindow.marker != marker)
-  // }
